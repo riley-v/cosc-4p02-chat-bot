7c3c8dc920f5e95bafe1fb0ec7133438dcdcde8f
riley-v <77628966+riley-v@users.noreply.github.com>
2022-04-27 14:38:13 -0400

    Add files via upload

--- nlp/chat.py
+++ nlp/chat.py
@@ -1,8 +1,10 @@
 import sys
-
+import io
+ 
 from chatbot import ChatBot
 
 bot = ChatBot()
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'latin-1')
 
 sentence = str(sys.argv[1])
 #sentence = "Who teaches Cosc 4p02?"
